f22e72d6c86509554215dcf580cb7f200c2f3499
Suneel Chandra Vanamu <66864503+Suneel1508@users.noreply.github.com>
2025-08-01 09:57:22 -0700

    Fix WebContainer Vite compatibility issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    host: true,
+    port: 3000,
+    strictPort: true,
+    hmr: {
+      port: 3001,
+    },
+  },
+  optimizeDeps: {
+    exclude: ['fsevents']
+  },
+  define: {
+    global: 'globalThis',
   }
 })
